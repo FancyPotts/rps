d62b3aa25ba369552aee98750c5d2bef4419d21f
pottsaf <aaronfpotts@gmail.com>
2023-03-16 07:11:23 -0600

    Make function run 5 rounds and show results after completed

--- myscripts.js
+++ myscripts.js
@@ -1,18 +1,28 @@
-function playRPS() {
-    const choices = ["rock", "paper", "scissors"];
-    const playerSelection = prompt("Choose your weapon: rock, paper or scissors.");
-    const computerSelection = choices[Math.floor((Math.random() * choices.length))];
-    if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissor" || playerSelection === "scissor" && computerSelection === "rock") {
-        console.log("You lose!")
-    } else if (playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissor" && computerSelection === "paper" || playerSelection === "rock" && computerSelection === "scissor") {
-        console.log("You win!");
-    } else {
-        console.log("Draw!");
+function playFiveRounds() {
+    let playerTally = 0
+    let computerTally = 0
+    for (let i = 1; i <= 5; i++) {
+        const choices = ["rock", "paper", "scissors"];
+        const playerSelection = prompt("Choose your weapon: rock, paper or scissors.");
+        console.log(playerSelection);
+        const computerSelection = choices[Math.floor((Math.random() * choices.length))];
+        console.log(computerSelection)
+        if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
+            console.log("You lose!");
+            computerTally++;
+            console.log(computerTally);
+        } else if (playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "rock" && computerSelection === "scissors") {
+            console.log("You win!");
+            playerTally++;
+            console.log(playerTally)
+        } else {
+            console.log("Draw!");
+        }
+ 
     }
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRPS()
-     }
+    if (playerTally > computerTally) {
+        console.log("Congratulations, you won by " + (playerTally - computerTally))
+        } else if (playerTally < computerTally) {
+        console.log("Ah boo, you lost by " + (computerTally - playerTally))
+    }
 }
