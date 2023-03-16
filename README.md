c429d68bc97910088cd78aad503f284b9a515d62
pottsaf <aaronfpotts@gmail.com>
2023-03-16 07:44:57 -0600

    Add things to readme checklist

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
   - &#x2611; return win, lose, draw based on input
 - &#x2611; create function to play 5 rounds
 - Modify function to continue game until one side wins at least 3 games.
+- Modify to change input to all lower-case.
+- Continue to ask for input if input is other than what is listed in array.
--- myscripts.js
+++ myscripts.js
@@ -1,20 +1,19 @@
 function playFiveRounds() {
     let playerTally = 0
     let computerTally = 0
+
     for (let i = 1; i <= 5; i++) {
         const choices = ["rock", "paper", "scissors"];
         const playerSelection = prompt("Choose your weapon: rock, paper or scissors.");
-        console.log(playerSelection);
         const computerSelection = choices[Math.floor((Math.random() * choices.length))];
-        console.log(computerSelection)
+
+        
         if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
             console.log("You lose!");
             computerTally++;
-            console.log(computerTally);
         } else if (playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "rock" && computerSelection === "scissors") {
             console.log("You win!");
             playerTally++;
-            console.log(playerTally)
         } else {
             console.log("Draw!");
         }
